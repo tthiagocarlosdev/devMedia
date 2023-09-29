b184f5aa0ae2e54eb3d881aefe8990336ddc5589
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2023-09-29 16:40:54 -0300

    missao_6: projeto-provedor - criacao do html da home page

---
+++ front-end/missao_6/projeto-provedor/especificacoes/script.js
@@ -0,0 +1,13 @@
+const abrirCaixaModal = document.getElementById("abrirCaixaModal");
+const caixaModal = document.getElementById("caixaModal");
+
+abrirCaixaModal.addEventListener("click", function () {
+    caixaModal.showModal();
+});
+
+const abrirCaixaParei = document.getElementById("abrirCaixaParei");
+const caixaParei = document.getElementById("caixaParei");
+
+abrirCaixaParei.addEventListener("click", function () {
+    caixaParei.showModal();
+});
